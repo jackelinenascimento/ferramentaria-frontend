df4494817e78202d934f7873e292e636977b5292
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-11-17 18:13:07 -0300

    reorganização de codigo

--- script/commons.js
+++ script/commons.js
@@ -2,3 +2,13 @@
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
+//menu
+function menu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}

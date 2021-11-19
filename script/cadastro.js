e94905c5821da1c47936e17db7ad24444c92de0d
Jackeline Nascimento <jackeline_nascimento@hotmail.com>
2021-11-19 11:23:57 -0300

    preechimento de dados a partir do cep

--- script/cadastro.js
+++ script/cadastro.js
@@ -22,25 +22,74 @@ function mascaraDeTelefone(telefone){
 
 }
 
-function mascaraDeCEP(cep) {
-    const textoAtual = cep.value;
-    let textoAjustado;
-    
-
-    const parte1 = textoAtual.slice(0,5);
-    const parte2 = textoAtual.slice(5,8);
-    textoAjustado = `${parte1}-${parte2}`;
-    
-    if (textoAjustado != "-"){
-        document.getElementById("CEP").value = textoAjustado;
-
-    }
-    
-}
-
 function tiraHifen(telefone) {
     const textoAtual = telefone.value;
     const textoAjustado = textoAtual.replace(/\-/g, '');
 
     return telefone.value = textoAjustado;
 }
+
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const logradouro = document.getElementById("logradouro");
+const complemento = document.getElementById("complemento");
+const bairro = document.getElementById("bairro");
+const cidade = document.getElementById("cidade");
+const uf = document.getElementById("uf");
+const cep = document.getElementById("cep");
+const telefone = document.getElementById("telefone");
+
+const usuario = {};
+
+cep.addEventListener("keypress", function (){ 
+    if(cep.value.length == 5)
+        cep.value = cep.value + '-'; 
+});
+
+function limpa_formulário_cep() {
+    logradouro.value=("");
+    complemento.value=("");
+    bairro.value=("");
+    cidade.value=("");
+    uf.value=("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        logradouro.value=(conteudo.logradouro);
+        complemento.value=(conteudo.complemento);
+        bairro.value=(conteudo.bairro);
+        cidade.value=(conteudo.localidade);
+        uf.value=(conteudo.uf);
+    }
+    else {
+        limpa_formulário_cep();
+        alert("CEP não encontrado.");
+    }
+}
+
+function pesquisacep(valor) {
+    var cep = valor.replace(/\D/g, '');
+    if (cep != "") {
+        var validacep = /^[0-9]{8}$/;
+        if(validacep.test(cep)) {
+            logradouro.value="...";
+            complemento.value="...";
+            bairro.value="...";
+            cidade.value="...";
+            uf.value="...";
+            var script = document.createElement('script');
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+            document.body.appendChild(script);
+        }
+        else {
+            limpa_formulário_cep();
+            alert("Formato de CEP inválido.");
+        }
+    }
+    else {
+        limpa_formulário_cep();
+    }
+};
